53ad10280ff1c3eb348632556cc64ad4e5037e3e
gulsute <134259376+gulsute@users.noreply.github.com>
2023-08-23 17:31:47 +0300

    dark mode tamamlandı

--- web-sayfam/src/components/Header.js
+++ web-sayfam/src/components/Header.js
@@ -1,19 +1,32 @@
+import { useState } from "react";
+
 export default function Header() {
+  const [darkMode, setDarkMode] = useState(true);
+  const darkLight = () => {
+    setDarkMode(!darkMode);
+    document.querySelector("html").classList.toggle("dark", darkMode);
+  };
+
   return (
-    <header class="flex justify-between items-center h-16 max-w-6xl m-auto py-4 px-6 ">
-      <div className="headLogo" class="flex-1">
-        <p class="text-[#7B61FF] bg-[#EEEBFF] w-14 h-14 rounded-full text-center py-3	font-semibold text-2xl rotate-[29deg] dark:bg-[#4731D3]">
-          G
-        </p>
+    <header>
+      <div className="darkMode" class="max-w-6xl m-auto py-6 flex-row-reverse">
+        <img onClick={darkLight} class="h-10" src="./dark-mode.png" />
       </div>
-      <div className="navbar">
-        <nav class="flex flex-1 gap-14 h-14 items-center text-lg text-[#6B7280] px-2">
-          <a>Yetenekler</a>
-          <a>Projeler</a>
-          <a class="border border-[#3730A3] rounded-md w-32 h-12 text-[#3730A3] text-center py-2 font-medium dark:bg-[#FFFFFF]">
-            Bana Ulaş
-          </a>
-        </nav>
+      <div class="flex justify-between items-center h-16 max-w-6xl m-auto py-4 px-6 ">
+        <div className="headLogo" class="flex-1">
+          <p class="text-[#7B61FF] bg-[#EEEBFF] w-14 h-14 rounded-full text-center py-3	font-semibold text-2xl rotate-[29deg] dark:bg-[#4731D3]">
+            G
+          </p>
+        </div>
+        <div className="navbar">
+          <nav class="flex flex-1 gap-14 h-14 items-center text-lg text-[#6B7280] px-2">
+            <a>Yetenekler</a>
+            <a>Projeler</a>
+            <a class="border border-[#3730A3] rounded-md w-32 h-12 text-[#3730A3] text-center py-2 font-medium dark:bg-[#FFFFFF]">
+              Bana Ulaş
+            </a>
+          </nav>
+        </div>
       </div>
     </header>
   );
